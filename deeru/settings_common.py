c05781ff97441f2b266289288a925d93629982a2
gojuukaze <ikaze_email@163.com>
2019-12-04 11:01:04 +0800

    增加编辑器高度

--- deeru/settings_common.py
+++ deeru/settings_common.py
@@ -149,7 +149,7 @@ DEERU_RICH_EDITOR = {
     'filed': 'app.ex_fields.fields.MFroalaField',
     'article_kwargs': {
         'options': {
-            'height': 300,
+            'height': 350,
             'toolbarButtons': ['fontFamily', 'fontSize', 'color', '|', 'paragraphFormat',
                                'paragraphStyle', 'bold', 'italic', 'underline', 'strikeThrough',
                                '|', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', '|',
@@ -161,7 +161,7 @@ DEERU_RICH_EDITOR = {
     },
     'flatpage_kwargs': {
         'options': {
-            'height': 300,
+            'height': 350,
             'toolbarButtons': ['fontFamily', 'fontSize', 'color', '|', 'paragraphFormat',
                                'paragraphStyle', 'bold', 'italic', 'underline', 'strikeThrough',
                                '|', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', '|',
